12ab2ce7a134014501be986059cfe473fe78f4c2
Erick Christian <erickchristian48@gmail.com>
2023-03-02 23:04:11 +0700

    fix(router): use hash router and add base name

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,34 +4,39 @@ import {
   Home,
   PromptsIntersection
 } from '@/modules'
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import App from './App'
 
-const router = createBrowserRouter([
+const router = createHashRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          errorElement: <ErrorPage />,
+          children: [
+            {
+              index: true,
+              element: <Home />
+            },
+            {
+              path: '/danbooru2prompt',
+              element: <Danbooru2Prompt />
+            },
+            {
+              path: '/prompts-intersection',
+              element: <PromptsIntersection />
+            }
+          ]
+        }
+      ]
+    }
+  ],
   {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true,
-            element: <Home />
-          },
-          {
-            path: '/danbooru2prompt',
-            element: <Danbooru2Prompt />
-          },
-          {
-            path: '/prompts-intersection',
-            element: <PromptsIntersection />
-          }
-        ]
-      }
-    ]
+    basename: import.meta.env.BASE_URL
   }
-])
+)
 
 export default router
